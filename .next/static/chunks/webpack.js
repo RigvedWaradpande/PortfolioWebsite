f2b3d5acb8ae81127a985696a5e609c2c3ba0f91
RigvedWaradpande <rigved.waradpande@gmail.com>
2024-07-29 22:06:47 +0530

    changed some stuff

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "56377022a95537fd"; }
+/******/ 		__webpack_require__.h = function() { return "602ae6038b2cc400"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.4a9ba7b564fd7963.hot-update.js
+++ .next/static/webpack/webpack.4a9ba7b564fd7963.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3cca34b88d182f29"; }
+/******/ 	__webpack_require__.h = function() { return "819e32fbc9052995"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
